d86683b3772583b646e37500166c842966521aa0
Cesar <cesarbiods@gmail.com>
2017-02-07 17:00:45 -0500

    Got started
    Set up git and gradle win IntelliJ

---
+++ src/main/java/com/github/cesarbiods/connectionReader.java
@@ -0,0 +1,48 @@
+package com.github.cesarbiods;
+/**
+ * Created by cesar on 2/2/17.
+ */
+
+import java.net.*;
+import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class connectionReader {
+
+    public static void main(String[] args) {
+
+        String output  = getUrlContents("https://www.ncdc.noaa.gov/cdo-web/api/v2/locations");
+            JsonParser parser = new JsonParser();
+            JsonObject json = parser.parse(output).getAsJsonObject();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String prettyOutput = gson.toJson(json);
+            System.out.println(prettyOutput);
+        }
+
+    private static String getUrlContents(String theUrl) {
+        StringBuilder content = new StringBuilder();
+
+        try {
+            URL url = new URL(theUrl);
+
+            URLConnection urlConnection = url.openConnection();
+            String token = "tGlObizdVindugbTAYgLidfwnKENPDUA";
+            urlConnection.setRequestProperty("Token", token);
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+}
