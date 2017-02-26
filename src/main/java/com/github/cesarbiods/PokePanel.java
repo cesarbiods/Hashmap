10e99a5235e687732378b3901cf203141d28f284
Cesar <cesarbiods@gmail.com>
2017-02-25 20:29:30 -0500

    Text entered is displayed to the user

--- src/main/java/com/github/cesarbiods/PokePanel.java
+++ src/main/java/com/github/cesarbiods/PokePanel.java
@@ -1,6 +1,7 @@
 package com.github.cesarbiods;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
@@ -8,14 +9,20 @@ import java.awt.event.ActionListener;
  */
 public class PokePanel extends JPanel{
     private JButton b;
+    private JTextField t;
+    private ActionListener a;
     public PokePanel() {
         b = new JButton("Search");
+        t = new JTextField(10);
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                JOptionPane.showMessageDialog(PokePanel.this,
+                        "Content of the text field:\n" + t.getText());
+            }
+        });
         add(b);
-
-        b.addActionListener(e -> System.out.println("Hello"));
+        add(t);
+        t.setToolTipText("Please enter a dex number between 1-9");
     }
-
-//    public String getEntry() {
-//
-//    }
 }
